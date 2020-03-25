a76bd268cb30ba32b35b210c21332befa3862853
Aibol Izbassar <aibolizbassarov@gmail.com>
2020-03-25 21:04:03 +0600

    first

--- DelegateOOP/Program.cs
+++ DelegateOOP/Program.cs
@@ -3,38 +3,50 @@ using System.Collections;
 
 namespace DelegateOOP
 {
+    public delegate void CarTempStatus(int n, string s);
     class Car 
     {
-        int _temperature;
+        public CarTempStatus TempStatus;
+        int _temperature = 45;
         string _name;
         int _tachoMeter;
-        int _max;
+        
         public int TachoMeter 
-        { 
-            get { return _tachoMeter; } 
+        {
             set { _tachoMeter = value; } 
         }
+
         public Car(string Name)
         {
             this._name = Name;
         }
-        public void UpTacho()
+        public void TachoTemp()
         {
+            if (_temperature >= 45 && _temperature < 90)
+            {
+                if (_tachoMeter < 2000) 
+                {
+                    _temperature -= 1;
+                }
+                if (_tachoMeter > 2000 && _tachoMeter < 4000)
+                {
+                    _temperature += 5;
+                }
+                if (_tachoMeter > 4000 && _tachoMeter < 7000)
+                {
+                    _temperature += 10;
+                }
+                Console.WriteLine($"{_name} Temperature: {_temperature}");
+            }
 
+            else
+            {
+                TempStatus(_temperature, _name);
+            }   
+            
         }
-
-        public void DownTacho()
-        {
-
-        }
-
 
     }
-
-
-
-
-    delegate string Mydelegate(string s);
     
 
     class Program
@@ -42,8 +54,26 @@ namespace DelegateOOP
        
         static void Main(string[] args)
         {
-            
-           
+            Car car = new Car("Toyota");
+            Car car2 = new Car("Audi");
+            Random r = new Random();
+            car.TachoMeter = r.Next(1000, 7000);
+            car2.TachoMeter = r.Next(2000, 7000);
+            car.TempStatus = PrintTempStatus;
+            car2.TempStatus = PrintTempStatus;
+
+            for (int i = 0; i < 8; i++)
+            {
+                Console.WriteLine($"*******Это итерация {i} *******");
+                car.TachoTemp();
+                car2.TachoTemp();
+               
+            }
+        }
+
+        static void PrintTempStatus(int temperature, string name)
+        {
+            Console.WriteLine($"{name} temperature is critical: {temperature}");
         }
     }
 }
