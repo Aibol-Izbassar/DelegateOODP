9b0c5f4c11a67bf25f7e5d8059f228ef12dc44e5
Aibol Izbassar <aibolizbassarov@gmail.com>
2020-03-25 17:59:51 +0600

    Добавьте файлы проекта.

---
+++ DelegateOOP/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+
+namespace DelegateOOP
+{
+    class Car 
+    {
+        int _temperature;
+        string _name;
+        int _tachoMeter;
+        int _max;
+        public int TachoMeter 
+        { 
+            get { return _tachoMeter; } 
+            set { _tachoMeter = value; } 
+        }
+        public Car(string Name)
+        {
+            this._name = Name;
+        }
+        public void UpTacho()
+        {
+
+        }
+
+        public void DownTacho()
+        {
+
+        }
+
+
+    }
+
+
+
+
+    delegate string Mydelegate(string s);
+    
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+           
+        }
+    }
+}
